7a130df589aa5a83e9ef014c4438d9481e6f9cc7
Victor <viqtorkibz@gmail.com>
2024-01-16 14:28:35 +0300

    completed assynment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 function addingEventListener() {
 }
+// index.js
+
+function addingEventListener() {
+    const input = document.getElementById('input');
+  
+    function clickAlert() {
+      alert('I was clicked!');
+    }
+  
+    input.addEventListener('click', clickAlert);
+  }
+  
+  // Call the outer function to execute addingEventListener() and activate the event listener
+  addingEventListener();
+  
